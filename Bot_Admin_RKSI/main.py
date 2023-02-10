5bdb8445279d27eb84f6dd6dc6f4fd5eb8592a0a
Ruslan <124144112+rusas161rus@users.noreply.github.com>
2023-02-10 03:07:41 +0300

    Update main.py

--- Bot_Admin_RKSI/main.py
+++ Bot_Admin_RKSI/main.py
@@ -3,12 +3,27 @@ from psycopg2 import Error
 import requests as requests
 import config
 import sqlconnect
-import requests as requests
 from bs4 import BeautifulSoup
 import pyperclip
 import telebot
 import logging
 from telebot import types
+
+# Админ лист
+
+class Admin_user_id():
+    def __init__(self, id_user, name):
+        self.id_user = id_user
+        self.name_user = name        
+   
+Admin_user_0 = Admin_user_id (648841533, "Вова")
+Admin_user_1 = Admin_user_id (658118472, "Руслан")
+Admin_user_2 = Admin_user_id (8350493, "Анатолий")
+
+Admin_list = {Admin_user_0.id_user, Admin_user_1.id_user, Admin_user_2.id_user}
+
+#[648841533]
+#[658118472] 
 
 logger=telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
@@ -20,44 +35,55 @@ bot=telebot.TeleBot(config.TOKEN)
 def button(message):        
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
     btm1 = types.KeyboardButton('Расписание')
-    btm5 = types.KeyboardButton('Поиск по времени')
-    btm2 = types.KeyboardButton('Изменить расписание')
+    btm4 = types.KeyboardButton('Расписание с сайта РКСИ')
+    btm5 = types.KeyboardButton('Поиск по времени')    
+    markup.add(btm4, btm1, btm5)
     btm3 = types.KeyboardButton('Обновить расписание в базе')
-    btm4 = types.KeyboardButton('Расписание с сайта РКСИ')           
-    markup.add(btm1, btm3, btm2, btm4, btm5)
+    btm2 = types.KeyboardButton('Изменить расписание')               
+    markup.add(btm3, btm2)
     bot.send_message(message.chat.id, 'Бот запущен!', reply_markup = markup)
 
 @bot.message_handler(content_types = ['text', 'document', 'photo', 'audio', 'video', 'voice']) 
-def send_text(message):
+def send_text(message):    
 
     if message.text == 'Обновить расписание в базе':
-        try: 
-            connection = psycopg2.connect(  user = sqlconnect.USER, 
-                                            password = sqlconnect.PASSWORD, 
-                                            host = sqlconnect.HOST, 
-                                            port = sqlconnect.PORT, 
-                                            database = sqlconnect.DATABASE)        
-            file1 = open('pars.txt')
-            cursor = connection.cursor()
-            cursor.execute (''' DELETE FROM Raspisanie ''')    
-            cursor = connection.cursor()
-            file1 = open('pars.txt', 'r')   
-            for line in file1:
-                cursor.execute("""INSERT INTO Raspisanie (TIME_S, TIME_P, PREDMET, PREPODOVATEL, KABINET) VALUES {}""".format(line.strip()))
-                print(line)
-                #print("Line{}: {}".format(count, line.strip()))
-            connection.commit()
-            print(cursor.rowcount, "1 Запись успешно Вставлена")
-        except (Exception, Error) as error:
-            print("Ошибка при работе с PostgreSQL", error)   
-        finally:
-            if connection:
-                cursor.close()
-                connection.close()
-                print("Соединение с PostgreSQL закрыто")
-
-        bot.send_message(message.chat.id, "Расписание в базе успешно обновлено".format(message.from_user, bot.get_me()),
-        parse_mode = 'html') 
+        while True:
+            try:                
+                
+                id_message_user = message.from_user.id
+            except ValueError:
+                print("Обновить расписание в базе") 
+            if id_message_user in Admin_list:
+                try:
+                    connection = psycopg2.connect(  user = sqlconnect.USER, 
+                                                    password = sqlconnect.PASSWORD, 
+                                                    host = sqlconnect.HOST, 
+                                                    port = sqlconnect.PORT, 
+                                                    database = sqlconnect.DATABASE)        
+                    file1 = open('pars.txt')
+                    cursor = connection.cursor()
+                    cursor.execute (''' DELETE FROM Raspisanie ''')    
+                    cursor = connection.cursor()
+                    file1 = open('pars.txt', 'r')   
+                    for line in file1:
+                        cursor.execute("""INSERT INTO Raspisanie (TIME_S, TIME_P, PREDMET, PREPODOVATEL, KABINET) VALUES {}""".format(line.strip()))
+                        print(line)
+                        #print("Line{}: {}".format(count, line.strip()))
+                    connection.commit()
+                    print(cursor.rowcount, "1 Запись успешно Вставлена")
+                    bot.send_message(message.chat.id, "Расписание в базе успешно обновлено".format(message.from_user, bot.get_me()),
+                    parse_mode = 'html')
+                except (Exception, Error) as error:
+                    print("Ошибка при работе с PostgreSQL", error)   
+                finally:
+                    if connection:
+                        cursor.close()
+                        connection.close()
+                        print("Соединение с PostgreSQL закрыто")
+                        break    
+            if id_message_user not in Admin_list:
+                bot.send_message(message.chat.id, "Что бы обновить расписание нужно быть админом")
+                break 
 
     if message.text == 'Расписание с сайта РКСИ':        
         with open ('pars.txt', 'w') as f:
@@ -151,87 +177,69 @@ def send_text(message):
 
     if message.text == 'Поиск по времени':
         markup = types.InlineKeyboardMarkup(row_width=2)
-        markup.add(types.InlineKeyboardButton("По Времени Начала", callback_data = 'nachalo'))
-        markup.add(types.InlineKeyboardButton("По Времени Конца", callback_data = 'conec'))
-        bot.send_message(message.chat.id, "Выберете кнопку", reply_markup = markup)  
+        btmin_s=types.InlineKeyboardButton("По Времени Начала", callback_data = 'nachalo')
+        btmin_p=types.InlineKeyboardButton("По Времени Конца", callback_data = 'conec')
+        markup.add(btmin_s, btmin_p)              
+        bot.send_message(message.chat.id, "Нажмите кнопку!", reply_markup = markup)  
                 
     if message.text == 'Изменить расписание':
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        markup.add(types.InlineKeyboardButton("Создать целую запись", callback_data = 'k'))
-        markup.add(types.InlineKeyboardButton("Время Начала", callback_data = 'a'))
-        markup.add(types.InlineKeyboardButton("Время Конца", callback_data = 'b'))
-        markup.add(types.InlineKeyboardButton("Дата", callback_data = 'c'))
-        markup.add(types.InlineKeyboardButton("Предмет", callback_data = 'd'))
-        markup.add(types.InlineKeyboardButton("Учитель", callback_data = 'e'))
-        markup.add(types.InlineKeyboardButton("Кабинет", callback_data = 'f'))
-        markup.add(types.InlineKeyboardButton("Удалить запись", callback_data = 'g')) 
-        #markup.add(types.InlineKeyboardButton("Обновить целую запись", callback_data = 'l'))    
-        bot.send_message(message.chat.id, "Выберете кнопку", reply_markup = markup)
-
+        while True:
+            try:                
+                id_message_user = message.from_user.id                                                           
+            except ValueError:
+                print("что то пошло не так")                        
+                continue        
+            if id_message_user in Admin_list:
+                markup = types.InlineKeyboardMarkup(row_width=3)
+                markup.add(types.InlineKeyboardButton("Создать целую запись", callback_data = 'k'))
+                btm_l_1 = types.InlineKeyboardButton("Время Начала", callback_data = 'a')
+                btm_l_2 = types.InlineKeyboardButton("Время Конца", callback_data = 'b')
+                markup.add(btm_l_1, btm_l_2)
+                markup.add(types.InlineKeyboardButton("Дата", callback_data = 'c'))
+                btm_l_3 = types.InlineKeyboardButton("Предмет", callback_data = 'd')
+                btm_l_4 = types.InlineKeyboardButton("Учитель", callback_data = 'e')
+                btm_l_5 = types.InlineKeyboardButton("Кабинет", callback_data = 'f')
+                markup.add(btm_l_3, btm_l_4, btm_l_5)
+                markup.add(types.InlineKeyboardButton("Удалить запись", callback_data = 'g')) 
+                #markup.add(types.InlineKeyboardButton("Обновить целую запись", callback_data = 'l'))    
+                bot.send_message(message.chat.id, "Выберете что хотите отредактировать", reply_markup = markup)
+                break
+            if id_message_user not in Admin_list:
+                bot.send_message(message.chat.id, "Что бы менять расписание нужно быть админом")
+                break    
+                    
 @bot.callback_query_handler(func = lambda call: True)
 def answer(call):    
-    if call.data == 'a':        
-        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('Начало')
-        markup_reply.add(bbtm_in)
+    if call.data == 'a': # Время Начала   
         msg = bot.send_message(call.message.chat.id, "Введите ИД записи")
         bot.register_next_step_handler(msg, create_request_message_0)
-    if call.data == 'b':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('Конец')
-        markup_reply.add(bbtm_in)
+    if call.data == 'b': # Время Конца      
         msg = bot.send_message(call.message.chat.id, "Введите ИД записи")
         bot.register_next_step_handler(msg, create_request_message_1)
-    if call.data == 'c':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('Дата')
-        markup_reply.add(bbtm_in)
+    if call.data == 'c': # Дата      
         msg = bot.send_message(call.message.chat.id, "Введите ИД записи")
         bot.register_next_step_handler(msg, create_request_message_2)
-    if call.data == 'd':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('Предмет')
-        markup_reply.add(bbtm_in)
+    if call.data == 'd': # Предмет      
         msg = bot.send_message(call.message.chat.id, "Введите ИД записи")
         bot.register_next_step_handler(msg, create_request_message_3)
-    if call.data == 'e':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('Учитель')
-        markup_reply.add(bbtm_in)
+    if call.data == 'e': # Учитель      
         msg = bot.send_message(call.message.chat.id, "Введите ИД записи")
         bot.register_next_step_handler(msg, create_request_message_4)
-    if call.data == 'f':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('Кабинет')
-        markup_reply.add(bbtm_in)
+    if call.data == 'f': # Кабинет     
         msg = bot.send_message(call.message.chat.id, "Введите ИД записи")
         bot.register_next_step_handler(msg, create_request_message_5)
-    if call.data == 'g':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('Удалить запись')
-        markup_reply.add(bbtm_in)
-        msg = bot.send_message(call.message.chat.id, "Введите ID записи")
+    if call.data == 'g': # Удалить запись      
+        msg = bot.send_message(call.message.chat.id, "Введите ИД записи")
         bot.register_next_step_handler(msg, create_request_delete_1)
-    if call.data == 'k':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('Создать целую запись')
-        markup_reply.add(bbtm_in)
+    if call.data == 'k': # Создать целую запись       
         msg = bot.send_message(call.message.chat.id, "Введите дату в формате гггг-мм-дд")
         bot.register_next_step_handler(msg, create_insrt_request_message_0)
-    if call.data == 'nachalo':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('По Времени Начала')
-        markup_reply.add(bbtm_in)
+    if call.data == 'nachalo':        
         msg = bot.send_message(call.message.chat.id, "Введите время начала с в формате хх:хх")
         bot.register_next_step_handler(msg, create_select_request_message_0)
-    if call.data == 'conec':
-        markup_reply=types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bbtm_in = types.KeyboardButton('По Времени Конца')
-        markup_reply.add(bbtm_in)
+    if call.data == 'conec':        
         msg = bot.send_message(call.message.chat.id, "Введите время конца с в формате хх:хх")
-        bot.register_next_step_handler(msg, create_select_request_message_1) 
-
-
+        bot.register_next_step_handler(msg, create_select_request_message_1)   
 
 # Функция поиска по времени
 def create_select_request_message_0(message):
@@ -509,13 +517,15 @@ def select_request_SQL_s_0(message):
             raspis=("|ИД=" + a + "|" + b + "|с-" + c + "|по-" + d + "\n\n|" + e + "|" + f + "|каб-" + g)
             #print("ИД =", row[0], "| Дата =", row[1], "| Время начала =", row[2], "| Время конца =", row[3], "| Предмет =", row[4], "| Преподователь =", row[5], "| Кабинет =", row[6])                      
             bot.send_message(message.chat.id, raspis)
+            bot.send_message(message.chat.id, "--------------------------------------------------------------".format(name = message.text))
     except (Exception, Error) as error:
         print("Ошибка при работе с PostgreSQL", error)
-        bot.send_message(message.chat.id, "Ошибка при работе с PostgreSQL".format(name = message.text))
+        bot.send_message(message.chat.id, "Ошибка при работе с PostgreSQL".format(name = message.text))                
     finally:                        
         if connection:                           
             cursor.close()
             connection.close()
+            bot.send_message(message.chat.id, "Поиск завершен!".format(name = message.text))
             print("Соединение с PostgreSQL закрыто")
 def select_request_SQL_s_1(message):
     try: 
@@ -541,13 +551,15 @@ def select_request_SQL_s_1(message):
             raspis=("|ИД=" + a + "|" + b + "|с-" + c + "|по-" + d + "\n\n|" + e + "|" + f + "|каб-" + g)
             #print("ИД =", row[0], "| Дата =", row[1], "| Время начала =", row[2], "| Время конца =", row[3], "| Предмет =", row[4], "| Преподователь =", row[5], "| Кабинет =", row[6])                      
             bot.send_message(message.chat.id, raspis)
+            bot.send_message(message.chat.id, "--------------------------------------------------------------".format(name = message.text))            
     except (Exception, Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
-        bot.send_message(message.chat.id, "Ошибка при работе с PostgreSQL".format(name = message.text))
+            print("Ошибка при работе с PostgreSQL", error)
+            bot.send_message(message.chat.id, "Ошибка при работе с PostgreSQL".format(name = message.text))
     finally:                        
         if connection:                           
             cursor.close()
             connection.close()
+            bot.send_message(message.chat.id, "Поиск завершен!".format(name = message.text))
             print("Соединение с PostgreSQL закрыто")
 
 bot.polling(none_stop = True, timeout = 30)
