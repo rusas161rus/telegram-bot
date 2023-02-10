590b8e9e50a8552cb7a44e76c4838253568a1636
Ruslan <124144112+rusas161rus@users.noreply.github.com>
2023-02-10 06:34:09 +0300

    Update config.py

--- Bot_CRM/config.py
+++ Bot_CRM/config.py
@@ -1,1 +1,23 @@
-TOKEN='your token'
+#Bot
+TOKEN='Your token'
+
+#sqlconnect
+USER="your data"                                
+PASSWORD="your data"
+HOST="your data"
+PORT="your data"
+DATABASE="your data"
+
+# Admin_list
+class Admin_user_id():
+    def __init__(self, id_user, name):
+        self.id_user = id_user
+        self.name_user = name
+
+Admin_user_0 = Admin_user_id (0000000, "xxxxxxxx")
+Admin_user_1 = Admin_user_id (0000000, "xxxxxxxx")
+Admin_user_2 = Admin_user_id (0000000, "xxxxxxxx")
+Admin_user_3 = Admin_user_id (0000000, "xxxxxxxx")
+Admin_user_4 = Admin_user_id (0000000, "xxxxxxxx")
+
+Admin_list = {Admin_user_0.id_user, Admin_user_1.id_user, Admin_user_2.id_user, Admin_user_3.id_user, Admin_user_4.id_user}
